97ba7eb39fcea70b7b6db16e674e5d02fe6db40a
Jf-Chen <485133380@qq.com>
2018-05-23 22:32:55 +0800

    predictclass
    期望得出最终结果

--- MLII/predictClass.m
+++ MLII/predictClass.m
@@ -1,4 +1,4 @@
-function [TypeResult,predictLabel] = predictClass(dataOriginPath,modelPath,leadway,frequency,correctway,extractway,typeA,typeB)
+function [TypeResult,predictLabel,allresult,finalResult] = predictClass(dataOriginPath,modelPath,leadway,frequency,correctway,extractway)
 %PREDICTCLASS 处理单个文件，预测其类型，使用9beats的model
 %   此处显示详细说明 ,newaccuracy,typeResultSize
 %   dataOriginPath是.mat文件的路径包含E:\icbeb\TrainingSet\A0001.mat，
@@ -50,23 +50,83 @@ correctedData = correctBaseline(correctway,origindata,frequency);
 % 答：加载所有分类器，投票决定出结果最少的那种，丢弃相关分类器，重复以上过程，直到最后一个不剩，上溯一次，得出分类结果
 % 加载所有分类器，得到36个结果，投票决定最少的那种
 % 重复多次，直到剩下一个或三个分类器
-leftType=[];
+allType=[];
 possibleType=[];
-
-% typeA=1;
-% typeB=2;
-tempModelName=['model',num2str(typeA),num2str(typeB)];
-tempModelPath=[modelPath,tempModelName];
-eval('load(tempModelPath);');
-% Te2typeLabel=ones(size(oneSignalFeature,1),1);
-Te2typeLabel=[-1];
-Te2type=oneSignalFeature;
-eval(['[predictLabel,accuracy,dec_values]=svmpredict(Te2typeLabel,Te2type,',tempModelName,')']);
-if predictLabel==-1
-    TypeResult=typeA;
-else
-    TypeResult=typeB;
+allresult=zeros(8,9);
+for k=1:8
+    for j=k+1:9
+        typeA=k;
+        typeB=j;
+        tempModelName=['model',num2str(typeA),num2str(typeB)];
+        tempModelPath=[modelPath,tempModelName];
+        eval('load(tempModelPath);');
+        % Te2typeLabel=ones(size(oneSignalFeature,1),1);
+        Te2typeLabel=[1]; %毫无意义
+        Te2type=oneSignalFeature;
+        eval(['[predictLabel,accuracy,dec_values]=svmpredict(Te2typeLabel,Te2type,',tempModelName,');']);
+        if predictLabel==1  %前提是typeA<typeB%如果结果不对改成-1
+            TypeResult=typeA;
+        else 
+            if predictLabel==-1
+                TypeResult=typeB;
+            end
+        end
+        allresult(typeA,typeB)=TypeResult;
+%         allresult(typeA,typeB)=TypeResult;
+    end    
 end
+
+%统计allresult结果中最少的，丢弃相关分类器结果
+% 如何利用好这一部分,循环9次，每次丢弃数量最少的那一种相关的分类器，如6最少且不为0，第六列置0，第6行置0
+%-------------------------------------------------------------------------------
+finalResult=0;
+for t=1:9 %循环9次
+    tempnum=zeros(1,9);
+    for k=1:9 %统计每个类型的数量
+        for i=1:8 %遍历行
+            for j=1:9 %遍历列
+                if allresult(i,j)==k
+                    tempnum(1,k)=tempnum(1,k)+1;
+                end
+            end
+        end
+    end
+    minType=0;minNum=9; %以防万一,minType=1;
+    if sum(tempnum)>0 %不能全为0吧
+        for x=1:9
+            if tempnum(1,x)<minNum &&tempnum(1,x)>0
+                minNum=tempnum(1,x);
+                minType=x;
+            end
+        end
+    end
+    if minType<9
+        allresult(minType,:)=0;
+    end
+    allresult(:,minType)=0;
+    if sum(sum(allresult,1),2)==0
+        finalResult=minType
+        break;
+    end
+end
+%------------------------------------------------------------------------------
+
+
+
+% % typeA=1;
+% % typeB=2;
+% tempModelName=['model',num2str(typeA),num2str(typeB)];
+% tempModelPath=[modelPath,tempModelName];
+% eval('load(tempModelPath);');
+% % Te2typeLabel=ones(size(oneSignalFeature,1),1);
+% Te2typeLabel=[-1];
+% Te2type=oneSignalFeature;
+% eval(['[predictLabel,accuracy,dec_values]=svmpredict(Te2typeLabel,Te2type,',tempModelName,')']);
+% if predictLabel==-1
+%     TypeResult=typeA;
+% else
+%     TypeResult=typeB;
+% end
 
 %-----------------------predict end-------------------------------------
 %  [TypeResult,predictLabel] = predictClass('E:\icbeb\TrainingSet\A0016.mat','E:\icbeb\programs\MLII\9beats\',2,500,1,1,1,2)
